e0dcb5c620513369d5ea5c213f1b8a23dff7400f
Shishupal Kumar <shishupalkr2595@gmail.com>
2021-08-11 17:43:58 +0530

    Add Gizmo News App

---
+++ block-BRaaao/code/news-app/src/components/Articles.js
@@ -0,0 +1,21 @@
+import articles from  "../data";
+import Article from "./Article";
+
+
+function Articles() {
+    return(
+        <div className="article-container">
+        <h2 className="text-5xl my-12 font-bold text-center">Atricles List</h2>
+        <section className="flex justify-between px-12 flex-wrap articles">
+            
+                {
+                    articles.map(article => < Article key = {article.publishedAt} {...article}/>)            
+                }
+               
+        </section>
+        </div>
+    )
+}
+
+
+export default Articles;
---
+++ block-BRaaao/code/news-app/src/components/Hero.js
@@ -0,0 +1,11 @@
+function Hero() {
+    return (
+        <>
+        <div className="flex mt-10 rounded">
+        <img src="/images/articles.jpg" alt="Article" className="w-full rounded"/>
+        </div>
+
+        </>
+    )
+}
+export default Hero;
